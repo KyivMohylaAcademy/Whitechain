5b37b718ce4d34d3eccfc2b89e503c228e61b93f
Artem Tarasenko <shabashab.04@gmail.com>
2025-04-24 18:36:19 +0300

    feat(contracts-voting): voting token and marketplace natspec docs

--- contracts-voting/contracts/ERC20VotingToken.sol
+++ contracts-voting/contracts/ERC20VotingToken.sol
@@ -4,32 +4,55 @@ pragma solidity =0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title ERC20VotingToken
+/// @author Artem Tarasenko (@shabashab)
+/// @notice ERC20 token with mint and burn functions restricted to a marketplace contract.
+/// @dev Inherits from OpenZeppelin's ERC20 and Ownable. Only the designated marketplace can mint or burn tokens.
 contract ERC20VotingToken is ERC20, Ownable {
-  address public marketplaceAddress;
+    /// @notice Address of the authorized marketplace contract.
+    address public marketplaceAddress;
 
-  error UnauthorizedAccount();
+    /// @notice Error thrown when a non-authorized account calls a restricted function.
+    error UnauthorizedAccount();
 
-  constructor(
-    string memory name,
-    string memory symbol
-  ) ERC20(name, symbol) Ownable(_msgSender()) { }
+    /// @notice Deploys the ERC20VotingToken contract.
+    /// @param name The name of the ERC20 token.
+    /// @param symbol The symbol of the ERC20 token.
+    /// @dev Initializes the ERC20 and Ownable contracts.
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) Ownable(_msgSender()) { }
 
-  function setMarketplaceAddress(address _marketplaceAddress) external onlyOwner {
-    marketplaceAddress = _marketplaceAddress;
-  }
-
-  function mint(address to, uint256 amount) external onlyMarketplace {
-    _mint(to, amount);
-  }
-
-  function burn(address from, uint256 amount) external onlyMarketplace {
-    _burn(from, amount);
-  }
-
-  modifier onlyMarketplace {
-    if(_msgSender() != marketplaceAddress) {
-      revert UnauthorizedAccount();
+    /// @notice Sets the address of the authorized marketplace contract.
+    /// @dev Only callable by the contract owner.
+    /// @param _marketplaceAddress The address of the marketplace contract.
+    function setMarketplaceAddress(address _marketplaceAddress) external onlyOwner {
+        marketplaceAddress = _marketplaceAddress;
     }
-    _;
-  }
+
+    /// @notice Mints new tokens to a specified address.
+    /// @dev Only callable by the authorized marketplace contract.
+    /// @param to The address to receive the minted tokens.
+    /// @param amount The number of tokens to mint.
+    function mint(address to, uint256 amount) external onlyMarketplace {
+        _mint(to, amount);
+    }
+
+    /// @notice Burns tokens from a specified address.
+    /// @dev Only callable by the authorized marketplace contract.
+    /// @param from The address from which tokens will be burned.
+    /// @param amount The number of tokens to burn.
+    function burn(address from, uint256 amount) external onlyMarketplace {
+        _burn(from, amount);
+    }
+
+    /// @notice Restricts function access to the authorized marketplace contract.
+    /// @dev Reverts with UnauthorizedAccount if called by any other address.
+    modifier onlyMarketplace {
+        if(_msgSender() != marketplaceAddress) {
+            revert UnauthorizedAccount();
+        }
+        _;
+    }
 }
--- contracts-voting/contracts/ERC20VotingTokenMarketplace.sol
+++ contracts-voting/contracts/ERC20VotingTokenMarketplace.sol
@@ -3,48 +3,80 @@ pragma solidity =0.8.24;
 
 import "./ERC20VotingToken.sol";
 
+/// @title ERC20VotingTokenMarketplace
+/// @author Artem Tarasenko (@shabashab)
+/// @notice Marketplace for buying and selling ERC20VotingToken tokens for Ether.
+/// @dev Handles minting and burning of voting tokens, and manages token pricing.
 contract ERC20VotingTokenMarketplace {
-  uint8 private constant WEI_DECIMALS = 18;
+    /// @notice Number of decimals for wei (Ether's smallest unit).
+    uint8 private constant WEI_DECIMALS = 18;
 
-  ERC20VotingToken public votingToken;
-  uint8 private votingTokenDecimals;
+    /// @notice The ERC20VotingToken contract instance.
+    ERC20VotingToken public votingToken;
 
-  uint256 public votingTokenPrice;
+    /// @notice Number of decimals for the voting token, basically a cache.
+    uint8 private votingTokenDecimals;
 
-  error MarketplaceInsufficientBalance();
-  error InsufficientValue(uint256 actualValue, uint256 requiredValue);
-  error InsufficientTokenBalance();
+    /// @notice Price of one voting token in wei.
+    uint256 public votingTokenPrice;
 
-  constructor(
-    address _votingTokenAddress,
-    uint256 _initialPrice
-  ) {
-    votingToken = ERC20VotingToken(_votingTokenAddress);
-    votingTokenDecimals = votingToken.decimals();
-    votingTokenPrice = _initialPrice;
-  }
+    /// @notice Error thrown when the contract does not have enough Ether for a withdrawal.
+    error MarketplaceInsufficientBalance();
 
-  function purchaseTokens(uint256 amount) external payable {
-    uint256 requiredValue = (amount * votingTokenPrice) / (10 ** votingTokenDecimals);
+    /// @notice Error thrown when the user sends insufficient Ether for purchasing tokens.
+    /// @param actualValue The amount of Ether sent.
+    /// @param requiredValue The required amount of Ether to complete the purchase.
+    error InsufficientValue(uint256 actualValue, uint256 requiredValue);
 
-    if (msg.value < requiredValue) {
-      revert InsufficientValue(amount, requiredValue);
+    /// @notice Error thrown when the user tries to sell more tokens than they own.
+    error InsufficientTokenBalance();
+
+    /// @notice Deploys the marketplace contract.
+    /// @param _votingTokenAddress The address of the ERC20VotingToken contract.
+    /// @param _initialPrice The initial price of one voting token in wei.
+    constructor(
+        address _votingTokenAddress,
+        uint256 _initialPrice
+    ) {
+        votingToken = ERC20VotingToken(_votingTokenAddress);
+        votingTokenDecimals = votingToken.decimals();
+        votingTokenPrice = _initialPrice;
     }
 
-    votingToken.mint(msg.sender, amount);
-  }
+    /// @notice Buy voting tokens by sending Ether.
+    /// @dev Mints the specified amount of tokens to the caller if enough Ether is sent.
+    /// @param amount The number of voting tokens to purchase.
+    /// @custom:reverts InsufficientValue if the sent Ether is less than the required amount.
+    function purchaseTokens(uint256 amount) external payable {
+        uint256 requiredValue = (amount * votingTokenPrice) / (10 ** votingTokenDecimals);
 
-  function sellTokens(uint256 amount) external {
-    uint256 sellValue = (amount * (10 ** WEI_DECIMALS)) / votingTokenPrice;
+        if (msg.value < requiredValue) {
+            revert InsufficientValue(msg.value, requiredValue);
+        }
 
-    if(votingToken.balanceOf(msg.sender) < amount) {
-      revert InsufficientTokenBalance();
+        votingToken.mint(msg.sender, amount);
     }
 
-    votingToken.burn(msg.sender, amount);
-    payable(msg.sender).transfer(sellValue);
-  }
+    /// @notice Sell voting tokens in exchange for Ether.
+    /// @dev Burns the specified amount of tokens from the caller and sends Ether in return.
+    /// @param amount The number of voting tokens to sell.
+    /// @custom:reverts InsufficientTokenBalance if the caller does not have enough tokens.
+    function sellTokens(uint256 amount) external {
+        uint256 sellValue = (amount * (10 ** WEI_DECIMALS)) / votingTokenPrice;
 
-  receive() external payable {}
-  fallback() external payable {}
+        if (votingToken.balanceOf(msg.sender) < amount) {
+            revert InsufficientTokenBalance();
+        }
+
+        votingToken.burn(msg.sender, amount);
+        payable(msg.sender).transfer(sellValue);
+    }
+
+    /// @notice Accept Ether sent directly to the contract.
+    /// @dev Allows the contract to receive plain Ether transfers.
+    receive() external payable {}
+
+    /// @notice Fallback function to accept Ether.
+    /// @dev Allows the contract to receive Ether with data or unknown function calls.
+    fallback() external payable {}
 }
